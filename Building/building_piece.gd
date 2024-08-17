88000762028b663b29fdbeb03493802fce8a1766
drew-abbo <drew.a.abbo@gmail.com>
2024-08-17 00:05:32 -0400

    got it kinda working

--- Building/building_piece.gd
+++ Building/building_piece.gd
@@ -1,50 +1,62 @@
-extends RigidBody2D
+class_name BuildingPiece
+extends Node
 
 
 ## Emits when the block contacts something
 signal building_piece_collided
 
-## The speed that the piece moves at left and right when controlled.
+@export_category("User Movement Speed")
 @export var left_right_move_speed: float = 500
 @export var up_down_move_speed: float = 500
 @export var rotate_speed: float = 10
 
+@export_category("Body")
+@export var mass_kg: float = 1:
+	set(new_mass):
+		print("Setting mass")
+		mass_kg = new_mass
+		rigid_body_2d.mass = new_mass
+
 var is_player_controlled: bool = true
 var is_locked: bool = false
 
+@onready var rigid_body_2d: RigidBody2D = $RigidBody2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var position: Vector2:
+	set(pos):
+		rigid_body_2d.position = pos
+	get:
+		return rigid_body_2d.position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	if is_player_controlled:
-		linear_velocity.x += (
+		rigid_body_2d.linear_velocity.x += (
 				Input.get_axis("move_left", "move_right") * left_right_move_speed * delta)
-		linear_velocity.y += (
+		rigid_body_2d.linear_velocity.y += (
 				Input.get_axis("move_up", "move_down") * up_down_move_speed * delta)
-		angular_velocity += (
+		rigid_body_2d.angular_velocity += (
 				Input.get_axis("rotate_left", "rotate_right") * rotate_speed * delta)
 
 
 # when it contacts something lock it in place and remove player control
-func _on_body_entered(_body: Node) -> void:
+func _on_rigid_body_2d_body_entered(_body: Node) -> void:
+	print("COLLISION!!!")
+
 	is_player_controlled = false;
 
 	# stop movement
-	linear_velocity = Vector2(0, 0);
-	angular_velocity = 0;
+	rigid_body_2d.linear_velocity = Vector2(0, 0);
+	rigid_body_2d.angular_velocity = 0;
 
 	# remove it's ability to collide
-	set_collision_layer_value(1, false)
-	set_collision_mask_value(1, false)
+	rigid_body_2d.set_collision_mask_value(1, false)
 
-	# make it so that this function doesn't get fired again if it regains the
-	# ability to collide
-	max_contacts_reported = 0
-	contact_monitor = false
+	# disable the ability for the signal to be sent in the future (even if it
+	# regains the ability to collide)
+	rigid_body_2d.call_deferred("set_contact_monitor", false)
+	rigid_body_2d.max_contacts_reported = 0
 
-	# send a signal that says that we hit something
+	# send the signal that says that we hit something
 	building_piece_collided.emit()
--- main.gd
+++ main.gd
@@ -7,17 +7,30 @@ extends Node2D
 ## How fast the camera should move at the start
 @export var verical_camera_move_speed: float = 10
 
+const ALL_PIECES: Array[Resource] = [
+	preload("res://Building/Pieces/test_piece.tscn"),
+]
 
 @onready var main_camera: Camera2D = $MainCamera
 
+var current_user_controlled_piece: BuildingPiece = null
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
-	pass # Replace with function body.
+	_spawn_new_building_piece()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	# move camera up every so often
+	# move camera up
 	main_camera.position.y -= verical_camera_move_speed * delta
 	verical_camera_move_speed *= 1 + (camera_move_speed_multiplier * delta)
+
+
+## Spawns a new building piece for the player
+func _spawn_new_building_piece() -> void:
+	current_user_controlled_piece = ALL_PIECES.pick_random().instantiate()
+	add_child(current_user_controlled_piece)
+
+	current_user_controlled_piece.connect("building_piece_collided", _spawn_new_building_piece)
+
+	current_user_controlled_piece.position = main_camera.position
