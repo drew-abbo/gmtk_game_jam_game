09b4fd0f71ee93220f54b83056fe8a837ccec49d
drew-abbo <drew.a.abbo@gmail.com>
2024-08-18 23:44:56 -0400

    cleaned some stuff up

--- Cutscene.gd
+++ Cutscene.gd
@@ -1,32 +1,25 @@
 extends Node2D
 
-const CUTSCENE_1 = preload("res://Assets/Cutscene 1.png")
-const CUTSCENE_2 = preload("res://Assets/Cutscene 2.png")
-const CUTSCENE_3 = preload("res://Assets/Cutscene 3.png")
-const CUTSCENE_4 = preload("res://Assets/Cutscene 4.png")
+const CUTSCENE_FRAME_1: Resource = preload("res://Assets/Cutscene 1.png")
+const CUTSCENE_FRAME_2: Resource = preload("res://Assets/Cutscene 2.png")
+const CUTSCENE_FRAME_3: Resource = preload("res://Assets/Cutscene 3.png")
+const CUTSCENE_FRAME_4: Resource = preload("res://Assets/Cutscene 4.png")
 
-@onready var sprite_2d = $Sprite2D
-@onready var timer = $Timer
+@onready var frame: Sprite2D = $Frame
 
-var frameCount := 0
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	sprite_2d.texture = CUTSCENE_1
+var frameCount: int = 1
+
+
+func _ready() -> void:
+	frame.texture = CUTSCENE_FRAME_1
 	frameCount = 1
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	print(frameCount)
-	if frameCount == 1:
-		sprite_2d.texture = CUTSCENE_1
-	if frameCount == 2:
-		sprite_2d.texture = CUTSCENE_2
-	if frameCount == 3:
-		sprite_2d.texture = CUTSCENE_3
-	if frameCount == 4:
-		sprite_2d.texture = CUTSCENE_4
-
-
-func _on_timer_timeout():
-	frameCount += 1 # Replace with function body.
+func _on_timer_timeout() -> void:
+	frameCount += 1
+	match frameCount:
+		1: frame.texture = CUTSCENE_FRAME_1
+		2: frame.texture = CUTSCENE_FRAME_2
+		3: frame.texture = CUTSCENE_FRAME_3
+		4: frame.texture = CUTSCENE_FRAME_4
+		5: get_tree().change_scene_to_file("res://game_intro.tscn")
--- game_intro.gd
+++ game_intro.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var timer: Timer = $SwitchSceneTimer
+@onready var frame_timer: Timer = $SwitchFrameTimer
 @onready var fade_in_fade_out: FadeInFadeOut = $FadeInFadeOut
 
 const PRE_DELAY: float = 1
@@ -13,8 +13,7 @@ func _ready() -> void:
 	fade_in_fade_out.schedule_fade_from_black(PRE_DELAY)	
 
 
-func _on_timer_timeout() -> void:
-	print("!")
+func _on_switch_frame_timer_timeout() -> void:
 	get_tree().change_scene_to_file("res://game.tscn")
 
 
@@ -22,4 +21,4 @@ func _on_fade_in_fade_out_fade_completed() -> void:
 	if not _fade_in_done:
 		_fade_in_done = true
 		fade_in_fade_out.schedule_fade_to_black(
-				timer.time_left - fade_in_fade_out.fade_time - POST_DELAY)	
+				frame_timer.time_left - fade_in_fade_out.fade_time - POST_DELAY)	
